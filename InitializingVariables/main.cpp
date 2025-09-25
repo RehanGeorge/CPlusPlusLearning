fa74e7c83552d43df1a3602d182cf04e9e8c9029
Rehan George <rehan.george@gmail.com>
2025-09-25 09:58:24 +0530

    Initialized

---
+++ InitializingVariables/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Enter the length and width of the room separated by a space: ";
+    
+    double length {0};
+    double width {0};
+    
+    cin >> length >> width;
+    
+    double area = length * width;
+    
+    cout << "The area of the room is " << area << endl;
+    
+    return 0;
+}
