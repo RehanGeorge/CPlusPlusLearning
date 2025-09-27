bf1072a90971daea1c98808e577358c4795c8388
Rehan George <rehan.george@gmail.com>
2025-09-27 20:39:33 +0530

    Add conditional

--- Expressions/main.cpp
+++ Expressions/main.cpp
@@ -5,5 +5,7 @@ int main(int argc, char **argv)
 	int number {0};
     number++;
     
-    printf("%d", number);
+    printf("%d\n", number);
+    
+    if (number > 0) printf("The number is %d", number);
 }
