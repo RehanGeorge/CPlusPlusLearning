8880e6f43427cc8895d73c942809c3c201e69d99
Rehan George <rehan.george@gmail.com>
2025-09-28 19:19:13 +0530

    Add static casting

--- Expressions/main.cpp
+++ Expressions/main.cpp
@@ -1,11 +1,30 @@
 #include <stdio.h>
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char **argv)
 {
-	int number {0};
-    number++;
+//	int number {0};
+//    number++;
+//    
+//    printf("%d\n", number);
+//    
+//    if (number > 0) printf("The number is %d", number);
+
+    int total {};
+    int num1{}, num2{}, num3{};
+    const int count{3};
     
-    printf("%d\n", number);
+    cout << "Enter 3 integers separated by spaces: ";
+    cin >> num1 >> num2 >> num3;
     
-    if (number > 0) printf("The number is %d", number);
+    total = num1 + num2 + num3;
+    
+    double average{0.0};
+    average = static_cast<double>(total) / count;
+    
+    cout << "The 3 numbers were: " << num1 << "," << num2 << "," << num3 << endl;
+    cout << "The sum of the numbers is: " << total << endl;
+    cout << "The average is: " << average << endl;
 }
