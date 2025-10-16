af0df1879d55f1b85ffb86ebe6a2ce36f5313cfd
Rehan George <rehan.george@gmail.com>
2025-10-16 11:36:16 +0530

    Dynamic Memory Allocation

--- Pointers/main.cpp
+++ Pointers/main.cpp
@@ -16,4 +16,26 @@ int main()
     
     p = nullptr;
     cout << p << endl;
+    
+    // Create int pointer
+    int *int_ptr {nullptr};
+    int_ptr = new int;
+    
+    cout << int_ptr << endl;
+    cout << &int_ptr << endl;
+    
+    delete int_ptr;
+    
+    // Create double pointer
+    size_t size {0};
+    double *temp_ptr {nullptr};
+    
+    cout << "How many temps ";
+    cin >> size;
+    
+    temp_ptr = new double[size];
+    
+    cout << temp_ptr << endl;
+    
+    delete [] temp_ptr;
 }
