3f3f4b4fbd0975c462d1b0a458f3d3031e8760c0
Rehan George <rehan.george@gmail.com>
2026-04-06 21:30:41 +0530

    Add public attributes and methods

--- ClassAndObjects/main.cpp
+++ ClassAndObjects/main.cpp
@@ -1,45 +1,57 @@
 #include <iostream>
 #include <string>
-#include <vector>
 
 using namespace std;
 
 class Player {
+public:
     // attributes
     string name {"Player"};
     int health {100};
     int xp {3};
     
     // methods
-    void talk(string);
+    void talk(string text_to_say) {cout << name << " says " << text_to_say << endl;}
     bool is_dead();
 };
 
 class Account {
+public:
     // attributes
     string name {"Account"};
     double balance {0.0};
 
     // methods
-    bool deposit(double);
-    bool withdraw(double);
+    bool deposit(double bal) {balance += bal; cout << "In deposit" << endl; return true; }
+    bool withdraw(double bal) {balance -= bal; cout << "In withdraw" << endl; return true; }
 };
 
 int main() {
     Account frank_account;
+    frank_account.name = "Frank's account";
+    frank_account.balance = 5000.0;
+    frank_account.deposit(1000.0);
+    frank_account.withdraw(500.0);
+    
     Account jim_account;
     
     
     Player frank;
+    frank.name = "Frank";
+    frank.health = 100;
+    frank.xp = 12;
+    frank.talk("Hello there");
+    
     Player hero;
     
     Player players[] {frank, hero};
     
-    vector<Player> player_vec {frank};
-    player_vec.push_back(hero);
     
+    Player *enemy = new Player;
+    (*enemy).name = "Enemy";
+    enemy->talk("Argh");
     
-    Player *enemy {nullptr};
+    delete enemy;
     enemy = new Player;
     
     delete enemy;
