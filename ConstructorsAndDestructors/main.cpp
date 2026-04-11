9a8c3e327f4aa85249a6fe4cccd9797a75857e38
Rehan George <rehan.george@gmail.com>
2026-04-11 16:05:24 +0530

    Put constructors and destructors in right file

--- ConstructorsAndDestructors/main.cpp
+++ ConstructorsAndDestructors/main.cpp
@@ -1,5 +1,32 @@
 #include <stdio.h>
 
+using namespace std;
+
+class Player
+{
+private:
+    std::string name;
+    int health;
+    int xp;
+public:
+    void set_name(std::string name_val) {
+        name = name_val;
+    }
+    // Overloaded Constructors
+    Player() {
+        cout << "No args constructor called" << endl;
+    }
+    Player(std::string name) {
+        cout << "String arg constructor called" << endl;
+    }
+    Player(std::string name, int health, int xp) {
+        cout << "Three args constructor called" << endl;
+    }
+    ~Player() {
+        cout << "Destructor called for " << name << endl;
+    }
+}
+
 int main(int argc, char **argv)
 {
 	printf("hello world\n");
