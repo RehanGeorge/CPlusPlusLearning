9ef2ffe6e19f827d299dc475cc3cd017f8de68e5
Rehan George <rehan.george@gmail.com>
2026-04-11 16:09:20 +0530

    Add constructors and destructors for different variables

--- ConstructorsAndDestructors/main.cpp
+++ ConstructorsAndDestructors/main.cpp
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -25,10 +26,30 @@ public:
     ~Player() {
         cout << "Destructor called for " << name << endl;
     }
-}
+};
 
-int main(int argc, char **argv)
+int main()
 {
-	printf("hello world\n");
-	return 0;
+	{
+        Player slayer;
+        slayer.set_name("Slayer");
+    }
+    
+    {
+        Player frank;
+        frank.set_name("Frank");
+        Player hero("Hero");
+        hero.set_name("Hero");
+        Player villain("Villain", 100, 12);
+        villain.set_name("Villain");
+    }
+    
+    Player *enemy = new Player;
+    enemy->set_name("Enemy");
+    
+    Player *level_boss = new Player("Level Boss", 1000, 300);
+    level_boss->set_name("Level Boss");
+    
+    delete enemy;
+    delete level_boss;
 }
