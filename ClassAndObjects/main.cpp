819b192d31fa956288a353c83965faf50d7c26a9
Rehan George <rehan.george@gmail.com>
2026-04-11 16:02:54 +0530

    Add Player class

--- ClassAndObjects/main.cpp
+++ ClassAndObjects/main.cpp
@@ -4,6 +4,31 @@
 
 using namespace std;
 
+class Player
+{
+private:
+    std::string name;
+    int health;
+    int xp;
+public:
+    void set_name(std::string name_val) {
+        name = name_val;
+    }
+    // Overloaded Constructors
+    Player() {
+        cout << "No args constructor called" << endl;
+    }
+    Player(std::string name) {
+        cout << "String arg constructor called" << endl;
+    }
+    Player(std::string name, int health, int xp) {
+        cout << "Three args constructor called" << endl;
+    }
+    ~Player() {
+        cout << "Destructor called for " << name << endl;
+    }
+}
+
 int main() {
     Account frank_account;
     frank_account.set_name("Frank's account");
